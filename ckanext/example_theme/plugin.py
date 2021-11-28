95b0a43396116cfbfcdca882cb9b17b906d8ca50
M. Uzair Afzal <studies9990@gmail.com>
2021-11-28 18:03:41 +0500

    Directory structure setup for index.html. Bug NOT resolved

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -6,3 +6,12 @@ class ExampleThemePlugin(plugins.SingletonPlugin):
 
     '''
     pass
+
+    def update_config(self, config):
+        # Adding this line overrides CKAN's default templates (.html jinja template files)
+        # and allows us to define our own custom templates and pages
+        
+        # The word 'templates' is the path to the folder, 
+        # relative to this file
+        
+        toolkit.add_template_directory(config, 'templates')
