b671c097c558283c0a5caabeeddb852fc13a1e50
M. Uzair Afzal <studies9990@gmail.com>
2021-12-04 19:10:16 +0500

    Static files (.css and .png) served successfully from public directory

--- ckanext/example_theme/plugin.py
+++ ckanext/example_theme/plugin.py
@@ -2,16 +2,14 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 class ExampleThemePlugin(plugins.SingletonPlugin):
-    '''An example theme plugin.
-
-    '''
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config):
-        # Adding this line overrides CKAN's default templates (.html jinja template files)
-        # and allows us to define our own custom templates and pages
-        
-        # The word 'templates' is the path to the folder, 
-        # relative to this file
-        
         toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+
+    def get_helpers(self):
+        return {
+
+        }
